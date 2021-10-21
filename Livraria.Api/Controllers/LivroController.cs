eb6102c907f0ecbb571f92796bc44ca9e6144652
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-10-21 11:53:27 -0300

    Add project files.

---
+++ Livraria.Api/Controllers/LivroController.cs
@@ -0,0 +1,69 @@
+﻿using Livraria.Domain.Commands.Inputs;
+using Livraria.Domain.Handlers;
+using Livraria.Domain.Interfaces.Respositories;
+using Livraria.Domain.Query;
+using Livraria.Infra.Interfaces.Commands;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Livraria.Api.Controllers
+{
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly ILivroRepository _repository;
+        private readonly LivroHandler _handler;
+
+        public LivroController(ILivroRepository repository, LivroHandler handler)
+        {
+            _repository = repository;
+            _handler = handler;
+        }
+
+        [HttpPost]
+        [Route("v1/livros")]
+        public ICommandResult InserirLivro([FromBody] AdicionarLivroCommand command)
+        {
+            var result = _handler.Handle(command);
+            return result;
+        }
+
+        [HttpPut]
+        [Route("v1/livros/{livroId:long}")]
+        public ICommandResult AtualizarLivro([FromRoute] long livroId, [FromBody] AtualizarLivroCommand command)
+        {
+            command.Id = livroId;
+
+            var result = _handler.Handle(command);
+            return result;
+        }
+
+        [HttpDelete]
+        [Route("v1/livros/{livroId:long}")]
+        public ICommandResult ExcluirLivro([FromRoute] long livroId, [FromBody] ExcluirLivroCommand command)
+        {
+            command.Id = livroId;
+
+            var result = _handler.Handle(command);
+            return result;
+        }
+
+        [HttpGet]
+        [Route("v1/livros")]
+        public IList<LivroQueryResult> ObterLivros()
+        {
+            var result = _repository.Listar();
+            return result;
+        }
+
+        [HttpGet]
+        [Route("v1/livros/{livroId:long}")]
+        public LivroQueryResult ObterLivro([FromRoute] long livroId)
+        {
+            var result = _repository.Obter(livroId);
+            return result;
+        }
+    }
+}
---
+++ Livraria.Domain/Commands/Inputs/ExcluirLivroCommand.cs
@@ -0,0 +1,29 @@
+﻿using Flunt.Notifications;
+using Livraria.Infra.Interfaces.Commands;
+using System;
+using System.Text.Json.Serialization;
+
+namespace Livraria.Domain.Commands.Inputs
+{
+    public class ExcluirLivroCommand : Notifiable, ICommandPadrao
+    {
+        [JsonIgnore]
+        public long Id { get; set; }
+
+        public bool ValidarCommad()
+        {
+            try
+            {
+                if (Id <= 0)
+                    AddNotification("Id", "Id precisa ser maior do que 0");
+
+                return Valid;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
